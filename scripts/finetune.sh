2abc28f13c18d2404ed6575b2e7eb15568baa342
Yash Sharma <yash.sharma200999@gmail.com>
2023-08-30 00:30:17 -0400

    changed finetune script to work on vicuna model from huggingface with projector downloaded

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -3,32 +3,34 @@
 # Uncomment and set the following variables correspondingly to run this script:
 
 ################## VICUNA ##################
-# PROMPT_VERSION=v1
+PROMPT_VERSION=v1
 # MODEL_VERSION="vicuna-v1-3-7b"
 ################## VICUNA ##################
 
 ################## LLaMA-2 ##################
 # PROMPT_VERSION="llava_llama_2"
-# MODEL_VERSION="llama-2-7b-chat"
+# MODEL_VERSION="liuhaotian/llava-llama-2-7b-chat-lightning-lora-preview"
+MODEL_VERSION="lmsys/vicuna-7b-v1.3"
+MODEL_NAME="$(basename $MODEL_VERSION)"
 ################## LLaMA-2 ##################
 
 deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
-    --model_name_or_path ./checkpoints/$MODEL_VERSION \
+    --model_name_or_path $MODEL_VERSION \
     --version $PROMPT_VERSION \
-    --data_path ./playground/data/llava_instruct_80k.json \
+    --data_path ./playground/data/llava_instruct_150k.json \
     --image_folder /path/to/coco/train2017 \
     --vision_tower openai/clip-vit-large-patch14 \
-    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_VERSION-pretrain/mm_projector.bin \
+    --pretrain_mm_mlp_adapter ./checkpoints/llava-$MODEL_NAME-pretrain/mm_projector.bin \
     --mm_vision_select_layer -2 \
     --mm_use_im_start_end False \
     --mm_use_im_patch_token False \
     --bf16 True \
     --output_dir ./checkpoints/llava-$MODEL_VERSION-finetune \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
